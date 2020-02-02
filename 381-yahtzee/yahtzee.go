cfafd1eb02e0ee7413619e6199fce17c08d4bbb7
Alex Jones <alexsjones@protonmail.com>
2020-02-02 13:45:55 +0000

    calculates the maxium score

---
+++ 381-yahtzee/yahtzee.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	roll := [5]int{6, 6, 6, 6, 6}
+	max := maxScore(roll)
+	fmt.Println(max)
+}
+
+//returns the maximum score
+func maxScore(roll [5]int) int {
+	counts := map[int]int{}
+	for _, value := range roll {
+		counts[value]++
+	}
+
+	scores := map[int]int{}
+
+	for key, value := range counts {
+		scores[key] = key * value
+	}
+
+	max := 0
+	for _, value := range scores {
+		if value > max {
+			max = value
+		}
+	}
+	return max
+}
